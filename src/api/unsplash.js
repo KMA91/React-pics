5526513bebf3f321265af68175a983ef936b11ce
Kevin Ma <kevin@Kevins-MacBook-Pro.local>
2019-05-01 18:39:10 -0700

    updating gitignore

--- src/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: "https://api.unsplash.com/search/photos",
-  headers: {
-    Authorization: "Client-ID 421de60eb8384d452f0c8a2d30aa5fb2e3d17141acab3e7048b60ad955a55f84"
-  }
-});
